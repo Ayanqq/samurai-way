d6ca1f4b36b62bfc1588ed6bd7b92fc6e4eee2c7
Ayanqq <nazbievayan16@gmail.com>
2024-03-07 14:03:44 +0500

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./Components/header/Header";
+import {Footer} from "./Components/footer/Footer";
+import {Technologies} from "./Components/Technologies/Technologies";
+
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App;
